88388dfe81d924f6d25be6890322fd2c611b82d1
Mahasundhari S <smahasundhari2006@gmail.com>
2025-12-23 10:48:13 +0530

    synchronous JK flip-flop module

---
+++ Assignment_3/JK_FF_syn.v
@@ -0,0 +1,16 @@
+module jk_ff_sync(
+input j, k, clk, rst,
+output reg q);
+always @(posedge clk)
+begin
+	if(rst)
+	q=1'b0;
+	else
+	case({j,k})
+		2'b00: q = q;
+		2'b01: q = 1'b0;
+		2'b10: q = 1'b1;
+		2'b11: q = -q;
+endcase
+end
+endmodule
