25b6f5bef4f396612e0f89f9fb2d97f0189262bf
Mahasundhari S <smahasundhari2006@gmail.com>
2026-01-05 19:45:28 +0530

    Create priority_decoder_3_8.v

---
+++ Assignment 8/priority_decoder_3_8.v
@@ -0,0 +1,15 @@
+module priority_decoder_3_8(
+    input  [2:0] i,
+    output [7:0] o
+);
+
+assign o[7] =  i[2] &  i[1] &  i[0];
+assign o[6] =  i[2] &  i[1] & ~i[0];
+assign o[5] =  i[2] & ~i[1];
+assign o[4] =  i[2] & ~i[1] & ~i[0];
+assign o[3] = ~i[2] &  i[1] &  i[0];
+assign o[2] = ~i[2] &  i[1] & ~i[0];
+assign o[1] = ~i[2] & ~i[1] &  i[0];
+assign o[0] = ~i[2] & ~i[1] & ~i[0];
+
+endmodule
