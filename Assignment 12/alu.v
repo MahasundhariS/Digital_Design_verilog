2b98399a8ba9d9dc730f20d732bdbbdcc9304c9e
Mahasundhari S <smahasundhari2006@gmail.com>
2026-01-13 23:25:03 +0530

    Fix ALU control signal and update operations

--- Assignment 12/alu.v
+++ Assignment 12/alu.v
@@ -1,21 +1,21 @@
-module alu(
+module alu (
 input [31:0] in1,
 input [31:0] in2,
-input [3:0] alu_ctr1,
+input [3:0] alu_ctrl,
 output reg [31:0] rslt,
 output zero);
 
 assign zero = (rslt == 32'b0);
 
 always @(*) begin
-case (alu_ctr1)
+case (alu_ctrl) 
 4'b0000: rslt = in1 + in2;
 4'b0001: rslt = in1 - in2;
 4'b0010: rslt = in1 & in2;
 4'b0011: rslt = in1 | in2;
 4'b0100: rslt = in1 ^ in2;
 4'b0101: rslt = in1 << in2[4:0];
-4'b0110: rslt = in1 + in2[4:0];
+4'b0110: rslt = in1 >> in2[4:0];
 default: rslt = 32'b0;
 endcase
 end
